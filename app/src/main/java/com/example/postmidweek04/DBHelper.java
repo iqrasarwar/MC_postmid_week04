ae6cafc67d36b81850ef47724f63ff0495756707
saddaqayyum57 <saddaqayyum@gmail.com>
2022-08-20 13:07:28 +0500

    db setup

--- app/src/main/java/com/example/postmidweek04/DBHelper.java
+++ app/src/main/java/com/example/postmidweek04/DBHelper.java
@@ -8,16 +8,28 @@ import org.jetbrains.annotations.Nullable;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    @Override public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public static final String STUDENT_ID = "StudentID";
+    public static final String STUDENT_NAME = "StudentName";
+    public static final String STUDENT_Class = "StudentClass";
+    public static final String STUDENT_ENROLL = "IsEnrolled";
+    public static final String STUDENT_TABLE = "StudentTable";
+
+    public DBHelper(@Nullable Context context)
+    {
+        super(context, "MyDB.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqdb) {
+        String createTableStatement = "CREATE TABLE " + STUDENT_TABLE + "(" +
+                STUDENT_ID + " Integer PRIMARY KEY AUTOINCREMENT, " + STUDENT_NAME + " Text, "
+                + STUDENT_Class + " Int, " + STUDENT_ENROLL + " BOOL) ";
+        sqdb.execSQL(createTableStatement);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
     }
 
-    public DBHelper(@Nullable Context context)
-    {
-        super(context, "MyDB.db", null, 1);
-    }
 
 }
